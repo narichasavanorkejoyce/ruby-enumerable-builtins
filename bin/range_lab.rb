0e9b0db6f65af0343737ad7670e3ec055ae5d20d
narichasavanorkejoyce <nari.savanorkejoyce@gmail.com>
2017-04-12 16:16:39 -0400

    Training notes

--- bin/range_lab.rb
+++ bin/range_lab.rb
@@ -1,3 +1,21 @@
 # frozen_string_literal: true
 
 # range as enumerable lab
+
+# calculate the sum of all the even numbers in a range
+
+# create an array with the numbers 1 through 20
+array = (1..20).to_a
+
+# identify the odd values
+# calculate the sum of these values
+array_odd = array.select { |n| n if n % 2 != 0 }
+array_odd_sum = array_odd.reduce(:+)
+
+# identify the even values
+# calculate the sum of these values
+array_even = array.select { |n| n if n % 2 == 0 }
+array_even_sum = array_even.reduce(:+)
+
+# use each_with_object to add all the numbers in the array
+array.each_with_object({:sum => 0}) {|i, hsh| hsh[:sum] += i}
